89b7c4667b8d7130b3c0e2c2112a9e46983a3520
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-06-12 20:53:48 -0300

    REMOVE: Remove configuração de armazenamento de arquivos estáticos do Whitenoise

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'config.urls'
