cfe2398b47d0f427d7b353999c80a7c7ff04cd0e
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-08-29 15:00:53 -0300

    feat: adiciona migração para alterar o campo de senha do usuário e atualiza o serializer de registro

--- artelie/serializers/register.py
+++ artelie/serializers/register.py
@@ -2,17 +2,16 @@ from rest_framework import serializers
 from artelie.models.user import User
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
+    password = serializers.CharField(write_only=True)
+    username = serializers.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'full_name', 'password']
+        fields = ['username', 'email', 'password', 'full_name']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            full_name=validated_data.get('full_name', '')
-        )
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
         return user
--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ router.register(r'users', UserViewSet, basename='user')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('/', include(router.urls)),
+    path('', include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("profile/", ProfileView.as_view(), name="user_profile"),
