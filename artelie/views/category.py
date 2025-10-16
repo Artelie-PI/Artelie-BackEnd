2df5d05d2bba6635eaab8e86b04e6f86705f6732
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-10-16 20:52:06 -0300

    feat: adiciona permissão IsAuthenticated nas views de Address, Brand, Cart, Category, Order, Product, Review, Supplier e User

--- artelie/views/category.py
+++ artelie/views/category.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from artelie.models import Category
 from artelie.serializers import CategorySerializer
 
--- artelie/views/supplier.py
+++ artelie/views/supplier.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from artelie.models import Supplier
 from artelie.serializers import SupplierSerializer
 
