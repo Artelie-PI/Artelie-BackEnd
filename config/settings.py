81aea6a94731ade572242673e4106f3eb60b6005
GuilhermeSchreiber7 <guilhermeschreieber2007@gmail.com>
2025-10-31 16:11:18 -0300

    feat: ajusta configuração do backend de email para suportar ambiente de desenvolvimento

--- config/settings.py
+++ config/settings.py
@@ -187,13 +187,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'artelieonlineweb@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'artelieonlineweb@gmail.com')
+if MODE == 'DEVELOPMENT':
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'artelieonlineweb@gmail.com')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+    DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'artelieonlineweb@gmail.com')
 
 
 
